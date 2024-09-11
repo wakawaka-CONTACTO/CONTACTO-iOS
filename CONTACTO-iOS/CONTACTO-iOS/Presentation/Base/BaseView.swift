0147629e9c90da6d3e8a020a9c42e354aef77261
정채은 <cce523@hanmail.net>
2024-09-11 15:53:48 +0900

    Setting [#3] 파일 추가 및 세팅

---
+++ CONTACTO-iOS/CONTACTO-iOS/Presentation/Base/BaseView.swift
@@ -0,0 +1,20 @@
+//
+//  BaseView.swift
+//  CONTACTO-iOS
+//
+//  Created by 정채은 on 9/11/24.
+//
+
+import UIKit
+
+class BaseView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
