591b3ec3fba521003ef463e370ff43335bc20c00
정채은 <cce523@hanmail.net>
2024-09-11 16:55:43 +0900

    Setting [#3] name 파일 추가 및 세팅

---
+++ CONTACTO-iOS/CONTACTO-iOS/Presentation/Onboarding/View/NameOnboardingView.swift
@@ -0,0 +1,20 @@
+//
+//  NameOnboardingView.swift
+//  CONTACTO-iOS
+//
+//  Created by 정채은 on 9/11/24.
+//
+
+import UIKit
+
+class NameOnboardingView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
