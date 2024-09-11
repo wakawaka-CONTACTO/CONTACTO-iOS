1de6ffe87416d06d097a20f0e304173349607061
정채은 <cce523@hanmail.net>
2024-09-11 15:54:10 +0900

    Feat [#3] BaseView, VC 추가

--- CONTACTO-iOS/CONTACTO-iOS/Presentation/Base/BaseViewController.swift
+++ CONTACTO-iOS/CONTACTO-iOS/Presentation/Base/BaseViewController.swift
@@ -8,22 +8,51 @@
 import UIKit
 
 class BaseViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    // MARK: Properties
+    
+    lazy private(set) var className: String = {
+        return type(of: self).description().components(separatedBy: ".").last ?? ""
+    }()
+    
+    // MARK: Initializing
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nil, bundle: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-    */
-
+    
+    deinit {
+        print("DEINIT: \(className)")
+    }
+    
+    // MARK: Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+        setDelegate()
+        hideKeyboardWhenTappedAround()
+    }
+    
+    // MARK: UI
+    func setUI() {
+        setStyle()
+        setLayout()
+    }
+    
+    func setStyle() {
+        view.backgroundColor = .black
+    }
+    
+    func setLayout() {
+        
+    }
+    
+    func setDelegate() {
+        
+    }
 }
