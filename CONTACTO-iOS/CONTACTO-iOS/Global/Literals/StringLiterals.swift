6406f253cee2d673373381376dc1c439863844d2
정채은 <cce523@hanmail.net>
2024-09-11 14:03:19 +0900

    Add [#1] StringLiterals 세팅

--- CONTACTO-iOS/CONTACTO-iOS/Global/Literals/StringLiterals.swift
+++ CONTACTO-iOS/CONTACTO-iOS/Global/Literals/StringLiterals.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+enum StringLiterals {
+    enum Login {
+        static let title = "Log in or sign up"
+    }
+    enum Onboarding {
+        
+    }
+    
+    enum Home {
+        
+    }
+    
+    enum Chat {
+        
+    }
+    
+    enum LikeList {
+        
+    }
+    
+    enum Profile {
+        
+    }
+}
