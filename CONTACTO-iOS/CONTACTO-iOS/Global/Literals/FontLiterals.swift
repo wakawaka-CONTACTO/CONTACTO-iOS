6b9d6af2295d1431d6726d1b71f53ee551988be1
정채은 <cce523@hanmail.net>
2024-09-11 13:56:04 +0900

    Add [#1] FontLiterals 추가

--- CONTACTO-iOS/CONTACTO-iOS/Global/Literals/FontLiterals.swift
+++ CONTACTO-iOS/CONTACTO-iOS/Global/Literals/FontLiterals.swift
@@ -5,4 +5,37 @@
 //  Created by 정채은 on 9/11/24.
 //
 
-import Foundation
+import UIKit
+
+// TODO: 추후 다시 나오면 수정
+enum FontName: String {
+    case pretendard
+}
+
+enum FontLevel {
+    case body
+}
+
+extension FontLevel {
+    
+    var fontWeight: String {
+        switch self {
+        case .body:
+            return FontName.pretendard.rawValue
+        }
+    }
+    
+    var fontSize: CGFloat {
+        switch self {
+        case .body:
+            return 10
+        }
+    }
+    
+    var lineHeight: CGFloat {
+        switch self {
+        case.body:
+            return 10
+        }
+    }
+}
