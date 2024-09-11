8b61ee5a5e983d0ed8aff60d1ccc582000081964
정채은 <cce523@hanmail.net>
2024-09-11 11:41:17 +0900

    Setting [#1] 프로젝트 생성

---
+++ CONTACTO-iOS/CONTACTO-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CONTACTO-iOS
+//
+//  Created by 정채은 on 9/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
