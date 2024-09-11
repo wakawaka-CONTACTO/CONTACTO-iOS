3a0424c04b6453c4898d2d676509903b313bdee2
정채은 <cce523@hanmail.net>
2024-09-11 15:55:23 +0900

    Add [#3] StringLiterals 추가

--- CONTACTO-iOS/CONTACTO-iOS/Global/Literals/StringLiterals.swift
+++ CONTACTO-iOS/CONTACTO-iOS/Global/Literals/StringLiterals.swift
@@ -10,7 +10,13 @@ import Foundation
 enum StringLiterals {
     enum Login {
         static let title = "Log in or sign up"
+        static let email = "E-mail"
+        static let continueButton = "Continue"
+        static let orLabel = "or"
+        static let appleButton = "Continue with Apple"
+        static let help = "Need help signing in?"
     }
+    
     enum Onboarding {
         
     }
