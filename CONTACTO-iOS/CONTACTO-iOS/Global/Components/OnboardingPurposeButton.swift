39d965d44dfcf264b766cbdf55425ea67958c84a
정채은 <cce523@hanmail.net>
2024-09-12 12:17:54 +0900

    Feat [#3] purpose 공통 버튼 구현

---
+++ CONTACTO-iOS/CONTACTO-iOS/Global/Components/OnboardingPurposeButton.swift
@@ -0,0 +1,71 @@
+//
+//  OnboardingPurposeButton.swift
+//  CONTACTO-iOS
+//
+//  Created by 정채은 on 9/12/24.
+//
+
+import UIKit
+
+import SnapKit
+
+final class OnboardingPurposeButton: UIButton {
+    var num = 0
+    var isTapped = false
+    
+    let nameArray = [StringLiterals.Onboarding.Purpose.getalong,
+                     StringLiterals.Onboarding.Purpose.collaborate,
+                     StringLiterals.Onboarding.Purpose.makenew,
+                     StringLiterals.Onboarding.Purpose.art,
+                     StringLiterals.Onboarding.Purpose.group]
+    
+    let colorArray: [UIColor] = [.ctsubred,
+                                 .ctsubpink,
+                                 .ctsubblue2,
+                                 .ctsubyellow2,
+                                 .ctsubgreen1]
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    init(num: Int) {
+        super.init(frame: .zero)
+        self.num = num
+        setStyle()
+        setLayout()
+        self.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+    }
+    
+    private func setStyle() {
+        self.setRoundBorder(borderColor: .ctblack, borderWidth: 1.5, cornerRadius: 0)
+        self.setBackgroundColor(.ctwhite, for: .normal)
+        self.setBackgroundColor(.ctwhite, for: .highlighted)
+        self.setTitle(nameArray[num], for: .normal)
+        self.setTitleColor(.ctblack, for: .normal)
+        self.titleLabel?.font = .fontContacto(.button)
+    }
+    
+    private func setLayout() {
+        self.snp.makeConstraints {
+            $0.width.equalTo(SizeLiterals.Screen.screenWidth - 32.adjustedWidth)
+            $0.height.equalTo(34.adjustedHeight)
+        }
+    }
+    
+    @objc private func buttonTapped() {
+        isTapped.toggle()
+        
+        if isTapped {
+            setBackgroundColor(colorArray[num], for: .normal)
+            setBackgroundColor(colorArray[num], for: .highlighted)
+        } else {
+            setBackgroundColor(.ctwhite, for: .normal)
+            setBackgroundColor(.ctwhite, for: .highlighted)
+        }
+    }
+}
