687b6a7ae655caed3adb5171a91e74ec06d89059
정채은 <cce523@hanmail.net>
2024-09-12 12:18:48 +0900

    Feat [#3] purpose UI 구현
    넘어가기 버튼 추후 구현

---
+++ CONTACTO-iOS/CONTACTO-iOS/Presentation/Onboarding/View/PurposeOnboardingView.swift
@@ -0,0 +1,118 @@
+//
+//  PurposeOnboardingView.swift
+//  CONTACTO-iOS
+//
+//  Created by 정채은 on 9/12/24.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class PurposeOnboardingView: BaseView {
+    
+    private let topBackgroundView = UIView()
+    private let topImageView = UIImageView()
+    private let titleLabel = UILabel()
+    private let descriptionLabel = UILabel()
+    let getButton = OnboardingPurposeButton(num: 0)
+    let collaboButton = OnboardingPurposeButton(num: 1)
+    let makeButton = OnboardingPurposeButton(num: 2)
+    let artButton = OnboardingPurposeButton(num: 3)
+    let groupButton = OnboardingPurposeButton(num: 4)
+    
+    let nextButton = OnboardingNextButton(count: 2)
+    
+    override func setStyle() {
+        self.backgroundColor = .ctmainblue
+        
+        topBackgroundView.do {
+            $0.backgroundColor = .ctsuborange
+        }
+        
+        topImageView.do {
+            $0.image = UIImage(resource: .onboardingTop)
+            $0.contentMode = .scaleAspectFit
+        }
+        
+        titleLabel.do {
+            $0.text = StringLiterals.Onboarding.Purpose.title
+            $0.textColor = .ctblack
+            $0.font = .fontContacto(.title1)
+            $0.textAlignment = .center
+            $0.numberOfLines = 0
+        }
+        
+        descriptionLabel.do {
+            $0.text = StringLiterals.Onboarding.Purpose.description
+            $0.textColor = .ctblack
+            $0.font = .fontContacto(.caption2)
+            $0.textAlignment = .center
+        }
+    }
+    
+    override func setLayout() {
+        addSubviews(topBackgroundView,
+                    topImageView,
+                    titleLabel,
+                    descriptionLabel,
+                    getButton,
+                    collaboButton,
+                    makeButton,
+                    artButton,
+                    groupButton,
+                    nextButton)
+        
+        topBackgroundView.snp.makeConstraints {
+            $0.top.width.equalToSuperview()
+            $0.height.equalTo(145.adjustedHeight)
+        }
+        
+        topImageView.snp.makeConstraints {
+            $0.top.equalTo(safeAreaLayoutGuide.snp.top).inset(26.adjustedHeight)
+            $0.leading.trailing.equalToSuperview().inset(18.adjustedWidth)
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(topImageView.snp.bottom).offset(41.adjustedHeight)
+            $0.centerX.equalToSuperview()
+        }
+        
+        descriptionLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(55.adjustedHeight)
+            $0.trailing.equalToSuperview().inset(16.adjustedWidth)
+        }
+        
+        getButton.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(16.adjustedWidth)
+            $0.top.equalTo(descriptionLabel.snp.bottom).offset(16.adjustedHeight)
+            $0.height.equalTo(34.adjustedHeight)
+        }
+        
+        collaboButton.snp.makeConstraints {
+            $0.leading.trailing.height.equalTo(getButton)
+            $0.top.equalTo(getButton.snp.bottom).offset(16.adjustedHeight)
+        }
+        
+        makeButton.snp.makeConstraints {
+            $0.leading.trailing.height.equalTo(getButton)
+            $0.top.equalTo(collaboButton.snp.bottom).offset(16.adjustedHeight)
+        }
+        
+        artButton.snp.makeConstraints {
+            $0.leading.trailing.height.equalTo(getButton)
+            $0.top.equalTo(makeButton.snp.bottom).offset(16.adjustedHeight)
+        }
+        
+        groupButton.snp.makeConstraints {
+            $0.leading.trailing.height.equalTo(getButton)
+            $0.top.equalTo(artButton.snp.bottom).offset(16.adjustedHeight)
+        }
+        
+        nextButton.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalTo(safeAreaLayoutGuide.snp.bottom).inset(55.adjustedHeight)
+        }
+    }
+}
---
+++ CONTACTO-iOS/CONTACTO-iOS/Presentation/Onboarding/ViewController/PurposeOnboardingViewController.swift
@@ -0,0 +1,45 @@
+//
+//  PurposeOnboardingViewController.swift
+//  CONTACTO-iOS
+//
+//  Created by 정채은 on 9/12/24.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class PurposeOnboardingViewController: BaseViewController {
+    
+    private let purposeOnboardingView = PurposeOnboardingView()
+    
+    // MARK: Navigation Function
+    override func setNavigationBar() {
+        self.navigationController?.navigationBar.isHidden = true
+        self.navigationController?.interactivePopGestureRecognizer?.delegate = self
+    }
+    
+    override func setLayout() {
+        view.addSubviews(purposeOnboardingView)
+        
+        purposeOnboardingView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    override func setAddTarget() {
+        purposeOnboardingView.nextButton.addTarget(self, action: #selector(nextButtonTapped), for: .touchUpInside)
+    }
+    
+    @objc private func nextButtonTapped() {
+//        let purposeOnboardingViewController = PurposeOnboardingViewController()
+//        self.navigationController?.pushViewController(purposeOnboardingViewController, animated: true)
+    }
+}
+
+extension PurposeOnboardingViewController: UIGestureRecognizerDelegate {
+    func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
+    }
+}
