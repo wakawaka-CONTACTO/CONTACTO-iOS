d937dc7b3fb828782e7b160ef18bf6355882dad9
정채은 <cce523@hanmail.net>
2024-09-13 17:30:42 +0900

    Feat [#3] 정규식 추가

---
+++ CONTACTO-iOS/CONTACTO-iOS/Global/Extensions/String+.swift
@@ -0,0 +1,23 @@
+//
+//  String+.swift
+//  CONTACTO-iOS
+//
+//  Created by 정채은 on 9/13/24.
+//
+
+import Foundation
+
+extension String {
+    /// emoji 제외
+    func hasCharacters() -> Bool {
+        do {
+            let regex = try NSRegularExpression(pattern: "^[0-9a-zA-Z가-힣ㄱ-ㅎㅏ-ㅣ`~!@#$%^&*()\\-_=+\\[{\\]}\\\\|;:'\",<.>/?\\s]$", options: .caseInsensitive)
+            if let _ = regex.firstMatch(in: self, options: NSRegularExpression.MatchingOptions.reportCompletion, range: NSMakeRange(0, self.count)) {
+                return true
+            }
+        }catch {
+            return false
+        }
+        return false
+    }
+}
