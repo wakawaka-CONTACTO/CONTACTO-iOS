6748a2c4651ecb9dad4f24ffb777ada8e4cd5686
정채은 <cce523@hanmail.net>
2024-09-17 11:19:55 +0900

    Fix [#3] button touch event fix

--- CONTACTO-iOS/CONTACTO-iOS/Global/Extensions/UIViewController+.swift
+++ CONTACTO-iOS/CONTACTO-iOS/Global/Extensions/UIViewController+.swift
@@ -10,7 +10,7 @@ import UIKit
 extension UIViewController {
     func hideKeyboardWhenTappedAround() {
         let tap = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
-        tap.cancelsTouchesInView = false
+        tap.cancelsTouchesInView = true
         view.addGestureRecognizer(tap)
     }
     
