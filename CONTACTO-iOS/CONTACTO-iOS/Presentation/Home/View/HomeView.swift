42bcf62cb22e15e9f2361e170cae6cef7086efbe
정채은 <cce523@hanmail.net>
2024-09-18 14:47:05 +0900

    Feat [#7] 홈뷰 UI 구현
    추후 레이아웃 수정

--- CONTACTO-iOS/CONTACTO-iOS/Presentation/Home/View/HomeView.swift
+++ CONTACTO-iOS/CONTACTO-iOS/Presentation/Home/View/HomeView.swift
@@ -11,7 +11,108 @@ import SnapKit
 import Then
 
 final class HomeView: BaseView {
+    private let topImageView = UIImageView()
+    
+    let portImageView = UIImageView()
+    
+    let profileButton = UIButton()
+    let profileTitle = UILabel()
+    let profileNameLabel = UILabel()
+    
+    let noButton = UIButton()
+    let yesButton = UIButton()
+    
     override func setStyle() {
-        self.backgroundColor = .ctmainpink
+        self.backgroundColor = .ctblack
+        
+        topImageView.do {
+            $0.image = .imgHomeTop
+            $0.contentMode = .scaleAspectFill
+        }
+        
+        portImageView.do {
+            $0.image = .imgex
+            $0.contentMode = .scaleAspectFit
+        }
+        
+        profileButton.do {
+            $0.setRoundBorder(borderColor: .ctblack, borderWidth: 1.5, cornerRadius: 0)
+            $0.setBackgroundColor(.ctmainblue, for: .normal)
+        }
+        
+        profileTitle.do {
+            $0.text = StringLiterals.Home.Main.title
+            $0.textColor = .ctblack
+            $0.font = .fontContacto(.caption6)
+            $0.textAlignment = .center
+        }
+        
+        profileNameLabel.do {
+            $0.text = "abcdefg"
+            $0.textColor = .ctblack
+            $0.font = .fontContacto(.button4)
+            $0.textAlignment = .center
+        }
+        
+        noButton.do {
+            $0.setImage(.icX, for: .normal)
+        }
+        
+        yesButton.do {
+            $0.setImage(.icO, for: .normal)
+        }
+    }
+    
+    override func setLayout() {
+        
+        self.addSubviews(topImageView,
+                         portImageView,
+                         profileButton,
+                         noButton,
+                         yesButton)
+        
+        profileButton.addSubviews(profileTitle,
+                                  profileNameLabel)
+        
+        topImageView.snp.makeConstraints {
+            $0.top.equalTo(safeAreaLayoutGuide.snp.top)
+            $0.leading.trailing.equalToSuperview().inset(17)
+            $0.height.equalTo(47.adjustedHeight)
+        }
+        
+        portImageView.snp.makeConstraints {
+            $0.height.equalTo(492.adjustedHeight)
+            $0.top.equalTo(topImageView.snp.bottom).offset(4.adjustedHeight)
+            $0.leading.trailing.equalToSuperview()
+        }
+        
+        profileButton.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(topImageView.snp.bottom).offset(248.adjustedHeight)
+            $0.height.equalTo(48.adjustedHeight)
+            $0.width.equalTo(212.adjustedWidth)
+        }
+        
+        profileTitle.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(3.adjustedHeight)
+            $0.centerX.equalToSuperview()
+        }
+        
+        profileNameLabel.snp.makeConstraints {
+            $0.bottom.equalToSuperview().inset(3.adjustedHeight)
+            $0.centerX.equalToSuperview()
+        }
+        
+        noButton.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(10)
+            $0.bottom.equalToSuperview()
+        }
+        
+        yesButton.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(10)
+            $0.bottom.equalTo(noButton)
+        }
+        
+        
     }
 }
--- CONTACTO-iOS/CONTACTO-iOS/Presentation/Home/ViewController/HomeViewController.swift
+++ CONTACTO-iOS/CONTACTO-iOS/Presentation/Home/ViewController/HomeViewController.swift
@@ -13,11 +13,46 @@ import Then
 final class HomeViewController: BaseViewController {
     let homeView = HomeView()
     
+    override func setNavigationBar() {
+        self.navigationController?.navigationBar.isHidden = true
+        setSwipeAction()
+    }
+    
     override func setLayout() {
+        
+        let safeAreaHeight = view.safeAreaInsets.bottom
+        let tabBarHeight = tabBarController?.tabBar.frame.height ?? 85
+        
         view.addSubviews(homeView)
         
         homeView.snp.makeConstraints {
-            $0.edges.equalToSuperview()
+            $0.top.leading.trailing.equalToSuperview()
+            $0.bottom.equalTo(safeAreaHeight).offset(-tabBarHeight)
+        }
+    }
+    
+    private func setSwipeAction() {
+        let leftSwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes(_:)))
+        let rightSwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipes(_:)))
+        
+        leftSwipeGestureRecognizer.direction = .left
+        rightSwipeGestureRecognizer.direction = .right
+        
+        homeView.portImageView.addGestureRecognizer(leftSwipeGestureRecognizer)
+        homeView.portImageView.addGestureRecognizer(rightSwipeGestureRecognizer)
+    }
+}
+
+extension HomeViewController {
+    @objc private func handleSwipes(_ sender: UISwipeGestureRecognizer) {
+        
+        switch sender.direction {
+        case .left:
+            print("left")
+        case .right:
+            print("right")
+        default:
+            print("기타")
         }
     }
 }
