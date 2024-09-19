4e10c4b598b41ea98dcc446ac088e58f58443d79
정채은 <cce523@hanmail.net>
2024-09-19 18:41:03 +0900

    Feat [#7] 프로필 디테일 cell 추가

--- CONTACTO-iOS/CONTACTO-iOS/Presentation/Home/Cell/ProfileTalentCollectionViewCell.swift
+++ CONTACTO-iOS/CONTACTO-iOS/Presentation/Home/Cell/ProfileTalentCollectionViewCell.swift
@@ -7,6 +7,59 @@
 
 import UIKit
 
-class ProfileTalentCollectionViewCell: UICollectionViewCell {
+import SnapKit
+import Then
+
+final class ProfileTalentCollectionViewCell: UICollectionViewCell {
     
+    let talentLabel = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUI() {
+        setStyle()
+        setLayout()
+    }
+    
+    private func setStyle() {
+        self.do {
+            $0.setRoundBorder(borderColor: .ctblack, borderWidth: 0.85, cornerRadius: 0)
+        }
+        
+        talentLabel.do {
+            $0.textColor = .ctblack
+            $0.font = .fontContacto(.button5)
+        }
+    }
+    
+    private func setLayout() {
+        self.addSubviews(talentLabel)
+        
+        talentLabel.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.top.bottom.leading.trailing.equalToSuperview().inset(3)
+        }
+    }
+    
+    func configData(category: String, title: String) {
+        switch category {
+        case "design":
+            self.backgroundColor = .ctsubpink
+        case "art":
+            self.backgroundColor = .ctsubblue1
+        case "media":
+            self.backgroundColor = .ctsubbrown
+        default:
+            self.backgroundColor = .ctsubpink
+        }
+        
+        talentLabel.text = title
+    }
 }
