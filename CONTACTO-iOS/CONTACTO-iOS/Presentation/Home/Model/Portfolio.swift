b8da8aa0ed04436773fedbb01207a9ea930eb3a2
정채은 <cce523@hanmail.net>
2024-09-19 18:40:43 +0900

    Feat [#7] Portfolio data 추가

--- CONTACTO-iOS/CONTACTO-iOS/Presentation/Home/Model/Portfolio.swift
+++ CONTACTO-iOS/CONTACTO-iOS/Presentation/Home/Model/Portfolio.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+struct Portfolio: Codable {
+    let image: [String]
+    let name: String
+    let talent: [Talent]
+    let description: String
+    let purpose: [Int]
+    let insta: String
+    let web: String?
+}
+
+extension Portfolio {
+    static func portDummy() -> Portfolio {
+        let port = Portfolio(
+            image: ["a", "b", "c"],
+            name: "Pacay Pacay",
+            talent: [Talent(talent: ["Branding", "Grapics", "Motion Grapics", "Illustration"], category: "design"),
+                     Talent(talent: ["Printmaking"], category: "art"),
+                     Talent(talent: ["Writing"], category: "media")],
+            description: "안녕하세요떵개입니다오늘먹방은정채은입니다",
+            purpose: [0, 3],
+            insta: "chaentopia",
+            web: "www.naver.com")
+        return port
+    }
+}
