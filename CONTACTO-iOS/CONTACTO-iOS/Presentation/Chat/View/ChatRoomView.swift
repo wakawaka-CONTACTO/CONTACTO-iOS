b189c39543c470e858f26e378c8501ae8bd8b612
chaentopia <cce523@hanmail.net>
2024-09-21 00:53:08 +0900

    Add [#8] chatRoom Literals Setting

---
+++ CONTACTO-iOS/CONTACTO-iOS/Presentation/Chat/View/ChatRoomView.swift
@@ -0,0 +1,20 @@
+//
+//  ChatRoomView.swift
+//  CONTACTO-iOS
+//
+//  Created by 정채은 on 9/21/24.
+//
+
+import UIKit
+
+class ChatRoomView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
