75dd636e8591291ad79755eb7e4ddf3217a325ae
chaentopia <cce523@hanmail.net>
2024-09-22 23:27:59 +0900

    Feat [#8] 채팅방 날짜 cell UI 구현

--- CONTACTO-iOS/CONTACTO-iOS/Presentation/Chat/Cell/ChatRoomDateCollectionViewCell.swift
+++ CONTACTO-iOS/CONTACTO-iOS/Presentation/Chat/Cell/ChatRoomDateCollectionViewCell.swift
@@ -7,6 +7,40 @@
 
 import UIKit
 
-class ChatRoomDateCollectionViewCell: UICollectionViewCell {
+import SnapKit
+import Then
+
+final class ChatRoomDateCollectionViewCell: UICollectionViewCell {
     
+    let dateLabel = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUI() {
+        setStyle()
+        setLayout()
+    }
+    
+    private func setStyle() {
+        dateLabel.do {
+            $0.text = "SEP.19.2024"
+            $0.font = .fontContacto(.caption2)
+            $0.textColor = .ctblack
+        }
+    }
+    
+    private func setLayout() {
+        self.addSubviews(dateLabel)
+        
+        dateLabel.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+    }
 }
