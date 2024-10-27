8bf3fd863191faed52460dde3ef163f69624f3bf
정채은 <cce523@hanmail.net>
2024-10-27 16:02:06 +0900

    [Chore] TestFlight, Version Update

--- CONTACTO-iOS/CONTACTO-iOS/Application/SceneDelegate.swift
+++ CONTACTO-iOS/CONTACTO-iOS/Application/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        let rootViewController = MainTabBarViewController()
+        let rootViewController = LoginViewController()
         let navigationController = UINavigationController(rootViewController: rootViewController)
         window.rootViewController = navigationController
         window.makeKeyAndVisible()
