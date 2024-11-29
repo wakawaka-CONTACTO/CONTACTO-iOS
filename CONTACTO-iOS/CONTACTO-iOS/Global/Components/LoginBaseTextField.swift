4648cc473aca9a0a9d7f570f5fc5a3e75f99cde2
정채은 <cce523@hanmail.net>
2024-11-29 21:35:52 +0900

    Add [#13] 로그인 Base TextField 세팅

---
+++ CONTACTO-iOS/CONTACTO-iOS/Global/Components/LoginBaseTextField.swift
@@ -0,0 +1,20 @@
+//
+//  LoginBaseTextField.swift
+//  CONTACTO-iOS
+//
+//  Created by 정채은 on 11/29/24.
+//
+
+import UIKit
+
+class LoginBaseTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
