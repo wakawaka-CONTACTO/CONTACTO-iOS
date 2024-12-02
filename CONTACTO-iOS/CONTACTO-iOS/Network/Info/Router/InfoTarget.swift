ba7d9e88f7ccb3bc3799667a3db413b0beefff77
정채은 <cce523@hanmail.net>
2024-12-02 22:27:24 +0900

    Feat [#13] 탈퇴 API 연결

--- CONTACTO-iOS/CONTACTO-iOS/Network/Info/Router/InfoTarget.swift
+++ CONTACTO-iOS/CONTACTO-iOS/Network/Info/Router/InfoTarget.swift
@@ -10,37 +10,44 @@ import Foundation
 import Alamofire
 
 enum InfoTarget {
-    
+    case deleteMe
 }
 
 extension InfoTarget: TargetType {
     var authorization: Authorization {
         switch self {
-            
+        case .deleteMe:
+            return .authorization
         }
     }
     
     var headerType: HTTPHeaderType {
         switch self {
+        case .deleteMe:
+            return .hasToken
             
         }
     }
     
     var method: HTTPMethod {
         switch self {
+        case .deleteMe:
+            return .delete
             
         }
     }
     
     var path: String {
         switch self {
-            
+        case .deleteMe:
+            return "/api/v1/users/me"
         }
     }
     
     var parameters: RequestParams {
         switch self {
-            
+        case .deleteMe:
+            return .requestPlain
         }
     }
 }
