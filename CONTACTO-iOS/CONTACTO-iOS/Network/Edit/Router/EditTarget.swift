7add3e27c1377fafc302170c4cfd891bbb027b2e
aristo <dkfud2121@naver.com>
2025-03-03 21:02:51 +0900

    feat: request form-data 형식 수정, error- userTalents, should not null

--- CONTACTO-iOS/CONTACTO-iOS/Network/Edit/Router/EditTarget.swift
+++ CONTACTO-iOS/CONTACTO-iOS/Network/Edit/Router/EditTarget.swift
@@ -76,20 +76,29 @@ extension EditRequestBodyDTO {
             
             if let portfolioImageUrl = self.portfolioImageUrl, !portfolioImageUrl.isEmpty {
                 print("portfolioImageUrl is not empty. Count: \(portfolioImageUrl.count)")
+                
+                var keys: [Int] = [] // keys 배열 생성
+                
                 for (index, image) in portfolioImageUrl.enumerated() {
                     print("Index: \(index), Photo Size: \(image.count) bytes")
-                    formData.append(image, withName: "portfolioImageUrl[\(index)]", fileName: "image\(index).jpg", mimeType: "image/jpeg")
+                    keys.append(index+1)
+                    formData.append(image, withName: "portfolioImages", fileName: "image\(index).jpg", mimeType: "image/jpeg")
+                }
+                
+                // keys[] 배열을 폼 데이터에 추가
+                for (index, key) in keys.enumerated() {
+                    formData.append("\(key)".data(using: .utf8) ?? Data(), withName: "keys")
                 }
             } else {
                 print("portfolioImageUrl is nil or empty")
             }
             
             for (index, purpose) in self.userPurposes.enumerated() {
-                formData.append("\(purpose)".data(using: .utf8) ?? Data(), withName: "userPurposes[\(index)]")
+                formData.append("\(purpose)".data(using: .utf8) ?? Data(), withName: "userPurposes")
             }
             
             for (index, talent) in self.userTalents.enumerated() {
-                formData.append(talent.data(using: .utf8) ?? Data(), withName: "userTalents[\(index)]")
+                formData.append(talent.data(using: .utf8) ?? Data(), withName: "userTalents")
             }
         }
     }
