51ac6bcb9a656d38826bcde0685482c03d2415c0
aristo <dkfud2121@naver.com>
2025-03-07 17:41:58 +0900

    Feat [#47] 프로필 수정 요청 시 기존 이미지와 새로운 이미지 구분하는 DTO 추가

---
+++ CONTACTO-iOS/CONTACTO-iOS/Network/Edit/DTO/Request/EditRequestDTO.swift
@@ -0,0 +1,22 @@
+//
+//  EditRequestDTO.swift
+//  CONTACTO-iOS
+//
+//  Created by 장아령 on 3/7/25.
+//
+
+import Foundation
+
+struct EditRequestDTO: Codable{
+    let username: String
+    let email, description, instagramId, password: String
+    let webUrl: String?
+    let userPurposes: [Int]
+    let userTalents: [String]
+    // 새로 업로드할 이미지 데이터와 해당 순번
+    let newPortfolioImages: [Data]?
+    let newImageKeys: [Int]?
+    // 기존 이미지 URL과 해당 순번
+    let existingPortfolioImageUrls: [String]?
+    let existingImageKeys: [Int]?
+}
