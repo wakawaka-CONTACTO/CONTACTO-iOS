0c7c5ad59829703aa3aa78af914c3cc8d6057486
aristo <dkfud2121@naver.com>
2025-04-04 19:20:41 +0900

    feat [#194] extensionAmplitudeSender 생성, 범용적인 sender class 생성

--- CONTACTO-iOS/CONTACTO-iOS/Global/Resources/Amplitude/EventSender/EventAmplitudeSender.swift
+++ CONTACTO-iOS/CONTACTO-iOS/Global/Resources/Amplitude/EventSender/EventAmplitudeSender.swift
@@ -11,3 +11,23 @@ public protocol EventAmplitudeSender {
     func sendAmpliLog(eventName: EventName)
     func sendAmpliLog(eventName: EventName, properties: [String: Any])
 }
+
+public class extensionAmplitudeSender: EventAmplitudeSender {
+    let eventView: EventView
+
+    init(eventView: EventView) {
+        self.eventView = eventView
+    }
+    
+    public func sendAmpliLog(eventName: EventName){
+        let info = EventInfo(event: eventView, eventName: eventName)
+
+        AmplitudeManager.amplitude.track(eventInfo: info)
+    }
+
+    public func sendAmpliLog(eventName: EventName, properties: [String: Any]){
+        let info = EventInfo(event: eventView, eventName: eventName)
+
+        AmplitudeManager.amplitude.track(eventInfo: info, properties: properties)
+    }
+}
