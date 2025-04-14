0ddb4db73bd9080973c6fc4e28a0df3f385a2227
aristo <dkfud2121@naver.com>
2025-04-14 17:11:57 +0900

    feat [#241] push alarm Service, update device token 요청 시 amplitude log send

--- CONTACTO-iOS/CONTACTO-iOS/Network/Alarm/Service/AlarmService.swift
+++ CONTACTO-iOS/CONTACTO-iOS/Network/Alarm/Service/AlarmService.swift
@@ -7,5 +7,6 @@ protocol AlarmServiceProtocol {
 final class AlarmService: APIRequestLoader<AlarmTarget>, AlarmServiceProtocol, AlarmAmplitudeSender {
     func updateDeviceToken(bodyDTO: DeviceTokenRequestDTO, completion: @escaping (NetworkResult<EmptyResponse>) -> Void) {
         fetchData(target: .updateDeviceToken(bodyDTO), responseData: EmptyResponse.self, completion: completion)
+        self.sendAmpliLog(eventName: EventName.UPDATE_DEVICE_TOKEN)
     }
 } 
